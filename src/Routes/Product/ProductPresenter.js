91500fa95252c7645bdc1301a758d89f37ff93db
henry <zizimoos@gmail.com>
2019-12-12 17:53:41 +0900

    ....

--- src/Routes/Product/ProductPresenter.js
+++ src/Routes/Product/ProductPresenter.js
@@ -125,39 +125,7 @@ const BunBae = styled.div`
   /* z-index: 5; */
 `;
 
-const TongJi = styled.div`
-  width: 0;
-  height: 0;
-  position: absolute;
-  top: 190px;
-  left: 225px;
-  transform: rotate(220deg) scale(1.5, 1.5);
-  transform-origin: 50% 100%;
-  /* transform: scale(3, 3); */
-  border-left: 75px solid transparent;
-  border-right: 75px solid transparent;
-  border-top: 110px solid yellowgreen;
-  border-radius: 50%;
-  /* z-index: 5; */
-`;
-
 const MoneCoreCircle = styled.div`
-  width: 150px;
-  height: 150px;
-  background: red;
-  border-radius: 50%;
-  margin: auto;
-  padding: 65px;
-  text-align: center;
-  z-index: 100;
-  @media only screen and (max-width: 768px) {
-    width: 75px;
-    height: 75px;
-    padding-top: 30px;
-  }
-`;
-
-const MoneCoreCircle1 = styled.div`
   width: 150px;
   height: 150px;
   background: red;
@@ -195,14 +163,11 @@ const ProductPresenter = () => (
     <ProductDiv>
       <BusinessPart>
         <SmsLmsMMSFaxEmail>
+          <JuBBsu></JuBBsu>
+          <BunBae></BunBae>
           <MoneCoreCircleFunction>
-            <JuBBsu></JuBBsu>
-            <BunBae></BunBae>
-            <TongJi></TongJi>
             <MoneCoreCircle>
-              <MoneCoreTitle>
-                <Focus></Focus>
-              </MoneCoreTitle>
+              <MoneCoreTitle>{/* <Focus></Focus> */}</MoneCoreTitle>
             </MoneCoreCircle>
           </MoneCoreCircleFunction>
         </SmsLmsMMSFaxEmail>
