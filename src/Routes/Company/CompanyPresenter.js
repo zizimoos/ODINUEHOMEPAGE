8250be6f5f0c7495fd89968d3850b81c71457b15
henry <zizimoos@gmail.com>
2020-01-21 12:13:48 +0900

    samsung add

--- src/Routes/Company/CompanyPresenter.js
+++ src/Routes/Company/CompanyPresenter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-import risa from "../../assets/backImage/risa.png";
 import assembly from "../../assets/campaign/assembly.jpg";
 import fishing from "../../assets/campaign/fishing_01.jpg";
 import greenPluged from "../../assets/campaign/greenPluged.jpg";
 import jejudo from "../../assets/campaign/jejudo.jpg";
 import allStar from "../../assets/campaign/KBOallstar.jpg";
+import samsungA from "../../assets/campaign/samsungAss.png";
 
 const Container = styled.div`
   width: 100vw;
@@ -76,17 +76,24 @@ const CampaignPeople = styled.div`
   background-size: cover;
   z-index: 10;
 `;
-
+const CampaignTitle = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 10px;
+  color: black;
+`;
 const CampainText = styled.div`
   width: 400px;
   height: 150px;
   padding-top: 70px;
-  padding: 70px 20px 10px 20px;
+  padding: 50px 20px 10px 20px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   font-size: 14px;
   text-align: justify;
-  color: black;
+  color: gray;
   background-color: white;
   z-index: 0;
 `;
@@ -99,7 +106,9 @@ const CompanyPresenterPresenter = () => (
         <CampaignItem>
           <CampaignPicFishing bgImage={assembly}></CampaignPicFishing>
           <CampaignPeople bgImage={assembly}></CampaignPeople>
+
           <CampainText>
+            <CampaignTitle>2월, 국회토론회</CampaignTitle>
             여의도 국회의원회관에 서 열린 ‘길거리 담배꽁초 어떻게 할 것인가?’
             토론회에서 , 해마다 4조 개비가 버려지는 있고 미세 플라스틱이자 화학
             폐기물인 담배꽁초 문제를 해결하기 위한 대안으로 시가랩을 제시했다.
@@ -109,6 +118,7 @@ const CompanyPresenterPresenter = () => (
           <CampaignPicFishing bgImage={fishing}></CampaignPicFishing>
           <CampaignPeople bgImage={fishing}></CampaignPeople>
           <CampainText>
+            <CampaignTitle>3월, 2019 국제 낚시 박람회 (BEXCO)</CampaignTitle>
             담배꽁초 무단투기로 인한 해양생태계파괴를 막기 위해 시가랩을
             사용하여 흡연자와 담배생산자가 담배꽁초를 모으고 수거하자는 메시지를
             낚시인들에게 알렸다.
@@ -120,6 +130,7 @@ const CompanyPresenterPresenter = () => (
           <CampaignPicFishing bgImage={greenPluged}></CampaignPicFishing>
           <CampaignPeople bgImage={greenPluged}></CampaignPeople>
           <CampainText>
+            <CampaignTitle>5월, GreedPluged Seoul</CampaignTitle>
             시가랩은 젊은이들의 대표 봄철 환경 페스티벌인 ‘그린플러그드
             서울(2019)”에 참가해서 20-30세대들로 부터 담배꽁초 문제 해결을 위한
             실천적 대안으로 큰 호응을 받았다.
@@ -129,6 +140,7 @@ const CompanyPresenterPresenter = () => (
           <CampaignPicFishing bgImage={allStar}></CampaignPicFishing>
           <CampaignPeople bgImage={allStar}></CampaignPeople>
           <CampainText>
+            <CampaignTitle>7월, 2019 프로야구 올스타전(창원)</CampaignTitle>
             프로야구 올스타전이 열리는 창원NC파크를 찾아가 20대 부터 70대까지
             다양한 연령층의 흡연자들에게 시가랩을 소개하였고, 전 연령층으로의
             확산 가능성을 확인했다.
@@ -140,6 +152,7 @@ const CompanyPresenterPresenter = () => (
           <CampaignPicFishing bgImage={jejudo}></CampaignPicFishing>
           <CampaignPeople bgImage={jejudo}></CampaignPeople>
           <CampainText>
+            <CampaignTitle>8월, 제주해변 캠페인</CampaignTitle>
             피서철인 8월에 제주도 7개 해수욕장에서 순차적으로 실시된 ‘시가랩
             캠페인’을 통해, 제주해변을 보존하고자 하는 제주도민과 전국에서 온
             피서객들의 의식, 그리고 ’시가랩 캠페인’의 성공 가능성을 확인 할 수
@@ -147,13 +160,12 @@ const CompanyPresenterPresenter = () => (
           </CampainText>
         </CampaignItem>
         <CampaignItem>
-          <CampaignPicFishing bgImage={risa}></CampaignPicFishing>
-          <CampaignPeople bgImage={risa}></CampaignPeople>
+          <CampaignPicFishing bgImage={samsungA}></CampaignPicFishing>
+          <CampaignPeople bgImage={samsungA}></CampaignPeople>
           <CampainText>
-            가나다라마바사아 자차카타파하가나다라마바사아
-            자차카타파하가나다라마바사아 자차카타파하가나다라마바사아
-            자차카타파하가나다라마바사아 자차카타파하가나다라마바사아
-            자차카타파하가나다라마바사아 자차카타파하
+            <CampaignTitle>8월, 삼성생명 MOU</CampaignTitle>
+            삼성생명 한남동 지점과 상호 업무 협조 양해각서를 체결하였다.
+            매너있는 흡연문화 정착에 상호 공감하고 노력하기로 하였다.
           </CampainText>
         </CampaignItem>
       </CampaignContainer>
